1520beefecaccd4fce347686d7104d4ec56c3a36
m0303 <serhey0800@yandex.ru>
2022-08-01 18:47:15 +0300

    f

--- components/AuthInfoOnFrontend.jsx
+++ components/AuthInfoOnFrontend.jsx
@@ -2,11 +2,13 @@ import {withAuthInfo} from '@propelauth/react';
 
 // user is automatically injected from withAuthInfo
 function AuthInfoOnFrontend({user}) {
-    return <span>
+    return <div style={{width: '100vw', height: '100vh'}}>
         <h2>User Info</h2>
-        {user && user.pictureUrl && <img src={user.pictureUrl} />}
-        <pre>user: {JSON.stringify(user, null, 2)}</pre>
-    </span>
+        
+        {user &&
+            <iframe width="100%" height="100%" src="https://www.addictinggames.com/embed/html5-games/19016" scrolling="no"></iframe>
+            }
+    </div>
 }
 
 export default withAuthInfo(AuthInfoOnFrontend);
