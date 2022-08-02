c6b13a251149cbb6dda044302bb2342ec75fed45
m0303 <serhey0800@yandex.ru>
2022-08-02 14:29:59 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <div>
             <Head>
-                <title>Alfredo</title>
+                <title>Alfredo 3D</title>
                 <meta name="description" content="Exebition expirience"/>
                 <link rel="icon" href="/favicon.ico"/>
                 <link rel="shortcut icon" href="/favicon.ico" />
