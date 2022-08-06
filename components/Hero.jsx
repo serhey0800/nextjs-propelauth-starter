eb4ca4be5075c9bbe6422821b05c665333e5acc4
m0303 <serhey0800@yandex.ru>
2022-08-06 12:16:49 +0300

    update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = ({redirectToSignupPage,redirectToLoginPage}) => {
   <div className='hero'>
     <h1>Alfredo Exhibition Expirience</h1>
     <p className="subtitle">We created virtual, interactive 3D exhibitions.</p>
-    <p style={{marginTop: '24px'}}>Register to visit it</p>
+    <p style={{marginTop: '24px'}}>Register to visit it!</p>
     <div className='btn-wrap'>
             <button onClick={redirectToSignupPage}>Signup</button>
             <button onClick={redirectToLoginPage}>Login</button>
